9207afe812e391a7286ae94e6f62bb71c816934d
AL <aamfahim@myseneca.ca>
2024-09-12 16:32:35 -0400

    update method with util function and docs

--- util/GroqInstance/index.js
+++ util/GroqInstance/index.js
@@ -1,9 +1,17 @@
-import Groq from "groq-sdk"
+/**
+ * Creates a new instance of Groq using the provided apiKey and baseURL.
+ * @param {string} apiKey - The API key for authentication.
+ * @param {string} baseURL - The base URL for the Groq instance.
+ * @returns {Groq} - The new instance of Groq.
+ * @throws {Error} - If apiKey or baseURL are missing or not defined properly.
+ */
+import Groq from "groq-sdk";
+import ArgsChecker from "../ArgsChecker";
 
 export default GroqInstance = (apiKey, baseURL) => {
-    if (arguments.length < 2 || arguments.some(arg => arg === undefined)) {
-        throw new Error('apiKey and baseUrl are required');
-    }
+    if (!ArgsChecker(arguments, 2)) {
+        throw new Error('apiKey and baseURL are missing or values are not defined properly');
+    }    
 
     return new Groq({
         apiKey: apiKey,
