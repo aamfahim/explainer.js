d9f09d576865872f6fe5862ce44f65d334bcc1d8
AL <aamfahim@myseneca.ca>
2024-09-12 12:05:02 -0400

    added sort algorithm examples

---
+++ examples/bubble_sort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+
+    do {
+        swapped = false;
+        for (let i = 0; i < n - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+
+    return arr;
+}
