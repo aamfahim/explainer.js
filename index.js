0c3d98a71e795d26c56f9ff65309a6828176bfa9
AL <aamfahim@myseneca.ca>
2024-09-13 11:07:44 -0400

    BugFix: taking both boolean and argument value

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ program
   .description('CLI tool to process a file output the code blocks with comments')
 
 program
-  .addOption(new Option('-a, --api-key [your-key]', 'define API key to use for processing defined in .env file').env('API_KEY').makeOptionMandatory())
-  .addOption(new Option('-b, --baseURL [url]', 'define the base URL to use for processing defined in .env file').default('https://api.groq.com/').env('BASE_URL'))
-  .addOption(new Option('-m, --model [model-name]',  'define the model to use for processing').default('llama-3.1-70b-versatile').env('MODEL_NAME'))
-  .addOption(new Option('-o, --output [file.type]', 'define an output file').default('explainer_output.txt'))
-  .addOption(new Option('-t, --temperature [number]', 'define temperature of chat completion between 0 to 2').default(1).argParser(parseFloat))
+  .addOption(new Option('-a, --api-key <your-key>', 'define API key to use for processing defined in .env file').env('API_KEY').makeOptionMandatory())
+  .addOption(new Option('-b, --baseURL <url>', 'define the base URL to use for processing defined in .env file').default('https://api.groq.com/').env('BASE_URL'))
+  .addOption(new Option('-m, --model <model-name>',  'define the model to use for processing').default('llama-3.1-70b-versatile').env('MODEL_NAME'))
+  .addOption(new Option('-o, --output <file>', 'define an output file'))
+  .addOption(new Option('-t, --temperature <number>', 'define temperature of chat completion between 0 to 2').default(1).argParser(parseFloat))
   
 
 
