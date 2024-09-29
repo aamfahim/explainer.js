20534b3cb639cb6b30f5733aec0d39199d258f81
Majd Al Mnayer <majd.mounayer@gmail.com>
2024-09-29 15:21:10 -0400

    doc: added JSDOC comments to TomlChecker

--- util/TomlChecker.js
+++ util/TomlChecker.js
@@ -3,6 +3,13 @@ import * as os from 'os';
 import path from 'path';
 import { readFileSync } from 'fs';
 
+/**
+ * Checks if the provided Toml file is valid and returns the options in it.
+ *
+ * @param {string} tomlFile - the full file path/name of the TOML file provided by the user.
+ * @returns {Record<string, any>} - An object containing as attributes all the options in the provided TOML file.
+ * @throws {Error} - If the file provided cannot be read, not a valid TOML file, or does not exist.
+ */
 const TomlChecker = (tomlFile) => {
     let config = {};
 
